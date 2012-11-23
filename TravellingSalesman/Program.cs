2c0be5a49f6bcdc82b0839350109a6da1aa551a9
James King <metapyziks@gmail.com>
2012-11-23 12:22:51 +0000

    Now produced

--- TravellingSalesman/Program.cs
+++ TravellingSalesman/Program.cs
@@ -10,11 +10,26 @@ namespace TravellingSalesman
 {
     class Program
     {
+        private static readonly char DSC = Path.DirectorySeparatorChar;
+
         static void Main( string[] args )
         {
-            String filePath = args.Length > 0 ? args[ 0 ]
-                : "TestFiles" + Path.DirectorySeparatorChar + "SAtestcase.txt";
+            String outDir = ( args.Length > 1 ? args[ 1 ] : null );
 
+            if( outDir != null && !Directory.Exists( outDir ) )
+                Directory.CreateDirectory( outDir );
+
+#if DEBUG
+            SearchSingle( args.Length > 0 ? args[0]
+                : "cityfiles" + DSC + "SAtestcase.txt", outdir );
+            Console.ReadKey();
+#else
+            SearchDirectory( args.Length > 0 ? args[0] : "cityfiles", outDir );
+#endif
+        }
+
+        static void SearchSingle( String filePath, String outDir = null )
+        {
             Console.WriteLine( "Loading file {0}", filePath );
             Graph graph = Graph.FromFile( filePath );
             Console.WriteLine( "Graph loaded: {0} has {1} vertices", graph.Name, graph.Size );
@@ -23,28 +38,23 @@ namespace TravellingSalesman
             Route route;
             Stopwatch stopwatch = new Stopwatch();
 
-            searcher = new TestConstrSearcher();
-            stopwatch.Start();
-            route = searcher.Search( graph, true );
-            stopwatch.Stop();
-            Console.WriteLine( "Search time: {0}ms", stopwatch.ElapsedMilliseconds );
-            Console.WriteLine( route.ToString() );
-
-            searcher = new ReversingSearcher();
-            stopwatch.Start();
-            route = searcher.Search( graph, true );
-            stopwatch.Stop();
-            Console.WriteLine( "Search time: {0}ms", stopwatch.ElapsedMilliseconds );
-            Console.WriteLine( route.ToString() );
-
             searcher = new TestConstrSearcher( new ReversingSearcher() );
             stopwatch.Start();
             route = searcher.Search( graph, true );
             stopwatch.Stop();
+            
             Console.WriteLine( "Search time: {0}ms", stopwatch.ElapsedMilliseconds );
             Console.WriteLine( route.ToString() );
 
-            Console.ReadKey();
+            if( outDir != null )
+                File.WriteAllText( outDir + DSC + "tour" + graph.Name + ".txt", route.ToString( true ) );
+        }
+
+        static void SearchDirectory( String directory, String outDir = null )
+        {
+            Console.WriteLine( "Loading directory {0}", directory );
+            foreach( String filePath in Directory.EnumerateFiles( directory ) )
+                SearchSingle( filePath, outDir );
         }
     }
 }
